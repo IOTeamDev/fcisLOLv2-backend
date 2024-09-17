fb9cc39eb13583f3dd5e6152fe90580572f4cf3f
Omar M. Hasan <hilariousbananas@proton.me>
2024-09-17 10:13:58 +0300

    feat: /material/accepted endpoint

---
+++ src/app/api/material/accept/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { verifyToken } from "@/utils/verifyToken";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const id = searchParams.get("id");
+  const accepted = searchParams.get("accepted");
+
+  if (!id || !accepted) {
+    return NextResponse.json(
+      { error: "ID and accepted status are required" },
+      { status: 400 }
+    );
+  }
+
+  if (!["true", "false"].includes(accepted)) {
+    return NextResponse.json(
+      { error: "Invalid accepted status" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const authHeader = request.headers.get("Authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const userDataFromToken = await verifyToken(token, { role: true });
+
+    if (userDataFromToken.role !== "ADMIN") {
+      return NextResponse.json(
+        { error: "Forbidden: Admin role required" },
+        { status: 403 }
+      );
+    }
+
+    const updatedMaterial = await prisma.material.update({
+      where: { id: Number(id) },
+      data: { accepted: accepted === "true" },
+    });
+
+    return NextResponse.json(updatedMaterial);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
